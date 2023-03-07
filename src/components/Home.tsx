9b3e853b1cf6f5f671d058916b875e3deca29227
Antonio <antoniovictorbordignon@outlook.com>
2023-03-07 16:27:22 -0300

    fix: footer responsiveness

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,28 +4,26 @@ import { Login } from "./Login";
 
 export function Home() {
     return(
-        <div>
             <div className="bg-[url('../assets/backgroundHome.jpg')] bg-cover bg-no-repeat h-screen">
                 <Header/>
                 <div className='flex'>
-                <div className="text-white text-center mx-auto w-[1000px]">
-                    <h1 
-                    className="text-6xl font-gotham font-bold mt-[400px] mx-10">
-                        Veja a sua história através das musicas que você escuta
-                    </h1>
-                    <p
-                    className="text-2xl font-poppins font-medium mt-16 mx-10 mb-20"
-                    >
-                        Explore estatísticas detalhadas, compare seus gostos com os de seus amigos e descubra novos artistas. Transforme seus dados em diversão e comece a explorar agora mesmo!
-                    </p>
-                    <div className='pb-20'>
-                        <Login
-                        buttonText="Entrar com Spotify"/>
+                    <div className="text-white text-center mx-auto w-[1000px]">
+                        <h1 
+                        className="text-6xl font-gotham font-bold mt-[400px] mx-10">
+                            Veja a sua história através das musicas que você escuta
+                        </h1>
+                        <p
+                        className="text-2xl font-poppins font-medium mt-16 mx-10 mb-20"
+                        >
+                            Explore estatísticas detalhadas, compare seus gostos com os de seus amigos e descubra novos artistas. Transforme seus dados em diversão e comece a explorar agora mesmo!
+                        </p>
+                        <div className='pb-20'>
+                            <Login
+                            buttonText="Entrar com Spotify"/>
+                        </div>
                     </div>
                 </div>
-                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
     )
 }
