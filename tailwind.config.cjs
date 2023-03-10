40fef846d685da20d9b810b2b2a5772d79627175
Antonio <antoniovictorbordignon@outlook.com>
2023-03-10 17:59:12 -0300

    feat: artist of the month grid

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,6 @@ module.exports = {
         textHeader: '#1E1E1E',
         bgFooter: '#171A20',
         bgMain: '#111112'
-
       },
       fontFamily: {
         poppins: ['Poppins'],
@@ -20,8 +19,10 @@ module.exports = {
       backgroundImage: {
         home: "url('/backgroundHome.jpg')",
         homeMobile: "url('/backgroundHomeMobile.png')",
-        gradientHome: "linear-gradient(180deg, rgba(17, 17, 18, 0) 23.85%, #111112 83.73%);"
-
+        monthArtist: "url('/duaLipabg.png')",
+        gradientHome: "linear-gradient(180deg, rgba(17, 17, 18, 0) 23.85%, #111112 83.73%);",
+        gradientImage: "linear-gradient(180deg, rgba(0, 0, 0, 0) 12.26%, rgba(0, 0, 0, 0.75) 60.01%);",
+        gradientGrid: "linear-gradient(180deg, #48313D 0%, #171A20 68.44%);"
       }
     },
   },
